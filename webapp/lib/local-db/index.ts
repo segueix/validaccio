9979e1f596498d715d3096e8795c511a286047c0
Segueix <danielmasdexaxars@gmail.com>
2026-07-21 22:19:11 +0200

    [002] Afegeix el repositori local versionat

---
+++ webapp/lib/local-db/index.ts
@@ -0,0 +1,13 @@
+export {
+  LOCAL_DATABASE_SCHEMA,
+  openLocalDatabase,
+  requestResult,
+  withTransaction,
+} from "./database.ts";
+export { metadataRepository, projectRepository } from "./repositories.ts";
+export {
+  normalizeProjectRecord,
+  PROJECT_DATA_VERSION,
+  type DatabaseMetadata,
+  type ProjectRecord,
+} from "./types.ts";
